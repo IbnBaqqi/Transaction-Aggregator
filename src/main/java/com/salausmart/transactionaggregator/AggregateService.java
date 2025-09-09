1a671616f7a77eec1ed99e78625d4b1c3001404d
Abdulbaki <Salausmart132@gmail.com>
2025-09-10 01:44:36 +0300

    sorting the response(unsorted..gotta sleep)

--- src/main/java/com/salausmart/transactionaggregator/AggregateService.java
+++ src/main/java/com/salausmart/transactionaggregator/AggregateService.java
@@ -15,12 +15,26 @@ public class AggregateService {
         this.restClient = restClient;
     }
 
-    public String aggregate(String account) {
+    public List<AccountResponseDto> aggregate(String account) {
 
-        List<AccountResponseDto> accounts = restClient.get()
+        List<AccountResponseDto> accounts1 = restClient.get()
                 .uri("http://localhost:8889/transactions?account=" + account)
                 .retrieve()
                 .body(new ParameterizedTypeReference<>() {});
+
+        List<AccountResponseDto> accounts2 = restClient.get()
+                .uri("http://localhost:8889/transactions?account=" + account)
+                .retrieve()
+                .body(new ParameterizedTypeReference<>() {});
+        var sortedAccount = sortAccounts(accounts1, accounts2);
+        return sortedAccount;
+    }
+
+    private List<AccountResponseDto> sortAccounts(List<AccountResponseDto> accounts1, List<AccountResponseDto> accounts2) {
+        accounts1.addAll(accounts2);
+
+//        var grouped = accounts1.stream().collect(Collectors.groupingBy(account -> account.getTimestamp()));
+        var sorted = accounts1.stream().sorted(account -> account.getTimestamp())
         return null;
     }
 }
